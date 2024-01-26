5a78cb5922812b77d33f5bc87ccd93079a41db07
b0bhat <ryanwu0308@gmail.com>
2024-01-25 20:17:55 -0800

    Added basic stats and variants for new destroyers

--- src/scripts/shipsystems/deus_StrikeBombardmentStats.java
+++ src/scripts/shipsystems/deus_StrikeBombardmentStats.java
@@ -6,15 +6,17 @@ import com.fs.starfarer.api.impl.combat.BaseShipSystemScript;
 public class deus_StrikeBombardmentStats extends BaseShipSystemScript {
 
 	public static final float DAMAGE_BONUS_PERCENT = 100f;
-	public static final float RANGE_MALUS_PERCENT = 50f;
-	public static final float EXTRA_DAMAGE_TAKEN_PERCENT = 100f;
+	public static final float RANGE_MALUS_PERCENT = 70f;
+	public static final float GUIDANCE_BONUS = 20f;
 	
 	public void apply(MutableShipStatsAPI stats, String id, State state, float effectLevel) {
 		
 		float bonusPercent = DAMAGE_BONUS_PERCENT * effectLevel;
 		float rangeMalusPercent = -RANGE_MALUS_PERCENT * effectLevel;
+		float guidanceBonusPercent = GUIDANCE_BONUS * effectLevel;
 		stats.getMissileWeaponDamageMult().modifyPercent(id, bonusPercent);
 		stats.getMissileWeaponRangeBonus().modifyPercent(id, rangeMalusPercent);
+		stats.getMissileGuidance().modifyPercent(id, guidanceBonusPercent);
 		
 		//float damageTakenPercent = EXTRA_DAMAGE_TAKEN_PERCENT * effectLevel;
 //		stats.getArmorDamageTakenMult().modifyPercent(id, damageTakenPercent);
@@ -28,6 +30,7 @@ public class deus_StrikeBombardmentStats extends BaseShipSystemScript {
 	public void unapply(MutableShipStatsAPI stats, String id) {
 		stats.getMissileWeaponDamageMult().unmodify(id);
 		stats.getMissileWeaponRangeBonus().unmodify(id);
+		stats.getMissileGuidance().unmodify(id);
 //		stats.getArmorDamageTakenMult().unmodify(id);
 //		stats.getHullDamageTakenMult().unmodify(id);
 //		stats.getShieldDamageTakenMult().unmodify(id);
@@ -37,15 +40,15 @@ public class deus_StrikeBombardmentStats extends BaseShipSystemScript {
 	
 	public StatusData getStatusData(int index, State state, float effectLevel) {
 		float bonusPercent = DAMAGE_BONUS_PERCENT * effectLevel;
+		float rangeMalusPercent = -RANGE_MALUS_PERCENT * effectLevel;
+		float guidanceBonusPercent = GUIDANCE_BONUS * effectLevel;
 		//float damageTakenPercent = EXTRA_DAMAGE_TAKEN_PERCENT * effectLevel;
 		if (index == 0) {
 			return new StatusData("+" + (int) bonusPercent + "% missile weapon damage" , false);
 		} else if (index == 1) {
-			//return new StatusData("+" + (int) damageTakenPercent + "% weapon/engine damage taken", false);
-			return null;
+			return new StatusData("-" + (int) rangeMalusPercent + "% missile range", true);
 		} else if (index == 2) {
-			//return new StatusData("shield damage taken +" + (int) damageTakenPercent + "%", true);
-			return null;
+			return new StatusData("+" + (int) guidanceBonusPercent + "% missile guidance improvement", false);
 		}
 		return null;
 	}
